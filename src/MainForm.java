465490d17a72bc99d33d184a1bdd7aae1619a02a
Petro Burka <petyaburka@gmail.com>
2019-01-16 01:50:41 +0200

    Add combo fields, change values

--- src/MainForm.java
+++ src/MainForm.java
@@ -4,22 +4,29 @@ import java.awt.event.ActionEvent;
 public class MainForm extends JFrame
 {
     private JButton sendBtn;
-    private JTextArea text1;
+    private JTextArea textQuery;
     private JPanel rootPanel;
-    private JComboBox comboBox1;
+    private JComboBox comboField;
+    private JComboBox comboTable;
+    private JComboBox comboQuery;
 
     public MainForm()
     {
+        initForm();
+    }
+
+    private void sendClick(ActionEvent ev)
+    {
+        textQuery.setText("Clicked");
+    }
+
+    private void initForm()
+    {
         add(rootPanel);
         setTitle("LAB1");
-        setSize(400, 400);
+        setSize(600, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         sendBtn.addActionListener(this::sendClick);
-    }
-
-    private void sendClick(ActionEvent event)
-    {
-        text1.setText("Clicked");
     }
 }
